833ed6702a580a377aacc426c15b0d5414e081af
Saloni-Gupta28 <140901394+Saloni-Gupta28@users.noreply.github.com>
2025-07-31 10:04:02 +0530

    Time: 48 ms (14.75%) | Memory: 55 MB (5.75%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -10,9 +10,9 @@ var createCounter = function(n) {
 };
 
 
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+
+ const counter = createCounter(10)
+ counter() // 10
+  counter() // 11
+  counter() // 12
+ 
